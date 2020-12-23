bc78d7dc2bb72b0b5be2118a47eb8e93ec486f74
Akash <akash300383@gmail.com>
2020-12-23 08:40:14 +0530

    Web Application

---
+++ Eclipse Projects/SimpleWebProject/src/com/Login.java
@@ -0,0 +1,50 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter pw = response.getWriter();
+		//pw.println("Welcome to Servlet Program");
+		String user = request.getParameter("user");
+		String pass = request.getParameter("pass");			//JDBC 
+		if(user.equals("Ravi") && pass.equals("123")) {
+			pw.println("Successfully Login");
+		}else {
+			pw.println("Failure try once again");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+	}
+
+}
